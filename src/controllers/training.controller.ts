e6322414347a7f212b18ffee5beb18f7902107d6
Malte2009 <malte.roesgen@gmail.com>
2025-06-11 05:49:33 +0200

    add endpoints to retrieve training logs and log details by ID

--- src/controllers/training.controller.ts
+++ src/controllers/training.controller.ts
@@ -22,3 +22,40 @@ export const createTraining = async (req: Request, res: Response): Promise<any> 
         return res.status(500).send("Internal Server Error");
     }
 }
+
+export const getTrainingLogs = async (req: Request, res: Response): Promise<any> => {
+    const userId: string = (req as any).userId;
+
+    if (!userId) return res.status(400).send("Bad Request");
+
+    try {
+        const trainingLogs = await prisma.trainingLog.findMany({
+            where: { userId: userId },
+            orderBy: { date: 'desc' },
+            include:  { exercises: { include: { sets: true } } }
+        });
+        return res.status(200).json(trainingLogs);
+    } catch (error) {
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
+export const getTrainingLogById = async (req: Request, res: Response): Promise<any> => {
+    const userId: string = (req as any).userId;
+    const trainingLogId: string = req.params.id;
+
+    if (!userId || !trainingLogId) return res.status(400).send("Bad Request");
+
+    try {
+        const trainingLog = await prisma.trainingLog.findUnique({
+            where: { id: trainingLogId, userId: userId },
+            include: { exercises: { include: { sets: true } } }
+        });
+
+        if (!trainingLog) return res.status(404).send("Training Log Not Found");
+
+        return res.status(200).json(trainingLog);
+    } catch (error) {
+        return res.status(500).send("Internal Server Error");
+    }
+}
--- src/routes/training.routes.ts
+++ src/routes/training.routes.ts
@@ -1,9 +1,11 @@
 import {Router} from 'express';
 import {authenticateToken} from "../middleware/auth.middleware";
-import {createTraining} from "../controllers/training.controller";
+import {createTraining, getTrainingLogById, getTrainingLogs} from "../controllers/training.controller";
 
 const router = Router();
 
+router.get("/getTrainingLogs", authenticateToken, getTrainingLogs);
+router.get("/getTrainingLog/:id", authenticateToken, getTrainingLogById);
 router.post('/createTraining', authenticateToken, createTraining);
 
 export default router;
