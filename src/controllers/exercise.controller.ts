62b5a3936fb0698e02c13e07bdf7fcc5e3da6329
Malte2009 <malte.roesgen@gmail.com>
2025-06-27 18:15:53 +0200

    feat: implement getExerciseById endpoint with error handling and user authentication

--- src/controllers/exercise.controller.ts
+++ src/controllers/exercise.controller.ts
@@ -22,6 +22,29 @@ export const getExerciseNames = async (req: AuthenticatedRequest, res: Response,
     }
 }
 
+export const getExerciseById = async (req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<any> => {
+    const userId = req.userId;
+
+    const exerciseId: string = req.params.id;
+
+    if (!userId) return res.status(401).send('Token missing');
+
+    if (!exerciseId) return res.status(400).send("Exercise ID is required");
+
+    try {
+        const exercise = await prisma.exerciseLog.findUnique({
+            where: { id: exerciseId, userId: userId },
+            include: { sets: true }
+        });
+
+        if (!exercise) return res.status(404).send("Exercise not found");
+
+        return res.status(200).json(exercise);
+    } catch (error) {
+        next(error);
+    }
+}
+
 export const changeExercise = async (req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<any> => {
     if (!req.body) return res.status(400).send("Bad Request");
 
--- src/routes/exercise.routes.ts
+++ src/routes/exercise.routes.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express';
 import {authenticateToken} from "../middleware/auth.middleware";
-import {getExerciseNames, changeExercise, createExercise, deleteExercise} from "../controllers/exercise.controller";
+import {getExerciseById, getExerciseNames, changeExercise, createExercise, deleteExercise} from "../controllers/exercise.controller";
 
 
 const router = Router();
 
 router.get("/getExerciseNames", authenticateToken, getExerciseNames);
+router.get("/getExercise/:id", authenticateToken, getExerciseById);
 router.patch("/changeExercise/:id", authenticateToken, changeExercise);
 router.post('/createExercise', authenticateToken, createExercise);
 router.delete("/deleteExercise/:id", authenticateToken, deleteExercise);
