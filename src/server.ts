4170259401baa13f1d93c905bb5321564dd9815d
Malte2009 <malte.roesgen@gmail.com>
2025-08-03 23:49:37 +0200

    fix: remove '/health-api' from server running log message for clarity

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import app from './app';
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 const server = app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}/health-api`);
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
 
 server.on('error', (error) => {
