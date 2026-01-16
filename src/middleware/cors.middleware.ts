f1e5e41d66ecb44dcf59331ac9313b129e437832
Malte <malte.roesgen@gmail.com>
2026-01-16 21:22:16 +0100

    set cors headers in development mode and created function setHeaders

--- src/middleware/cors.middleware.ts
+++ src/middleware/cors.middleware.ts
@@ -5,6 +5,10 @@ export default function cors(req: Request, res: Response, next: NextFunction) {
     const origin = req.headers.origin;
 
     if (process.env.NODE_ENV !== "production") {
+        setHeaders(req, res, next);
+
+        if (req.method === "OPTIONS") return res.sendStatus(204);
+
         return next();
     }
 
@@ -13,18 +17,24 @@ export default function cors(req: Request, res: Response, next: NextFunction) {
     }
 
     if (allowedOrigins.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin);
-        res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-        res.header("Access-Control-Allow-Credentials", "true");
-        res.header("Vary", "Origin");
+        setHeaders(req, res, next);
 
-        if (req.method === "OPTIONS") {
-            return res.sendStatus(204);
-        }
+        if (req.method === "OPTIONS") return res.sendStatus(204);
+
 
         return next();
     }
 
     return res.status(403).send("Forbidden");
+}
+
+
+function setHeaders(req: Request, res: Response) {
+    const origin = req.headers.origin || "";
+
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Vary", "Origin");
 }
