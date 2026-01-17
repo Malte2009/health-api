44e5fabd48a12bbd7077edcd7432ef74d149c8b5
Malte <malte.roesgen@gmail.com>
2026-01-17 17:54:59 +0100

    enhance security with Helmet configuration

--- src/app.ts
+++ src/app.ts
@@ -39,7 +39,34 @@ const isAuthenticatedLimiter = rateLimit({
 
 app.use(cookieParser());
 app.use(express.json( { limit: '10mb' }));
-app.use(helmet())
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'"],
+            styleSrc: ["'self'", "'unsafe-inline'"],
+            imgSrc: ["'self'", "data:"],
+            connectSrc: ["'self'"],
+            fontSrc: ["'self'"],
+            objectSrc: ["'none'"],
+            mediaSrc: ["'self'"],
+            frameSrc: ["'none'"],
+        },
+    },
+    crossOriginEmbedderPolicy: true,
+    crossOriginOpenerPolicy: true,
+    crossOriginResourcePolicy: { policy: "same-origin" },
+    dnsPrefetchControl: { allow: false },
+    frameguard: { action: "deny" },
+    hidePoweredBy: true,
+    hsts: { maxAge: 31536000, includeSubDomains: true, preload: true },
+    ieNoOpen: true,
+    noSniff: true,
+    originAgentCluster: true,
+    permittedCrossDomainPolicies: { permittedPolicies: "none" },
+    referrerPolicy: { policy: "strict-origin-when-cross-origin" },
+    xssFilter: true,
+}))
 
 if (!process.env.ALLOWED_ORIGINS) {
 	console.error("ALLOWED_ORIGINS is not defined in the environment variables.");
